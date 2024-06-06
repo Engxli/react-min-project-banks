900e8904f047c655d6b9b0b6e79e86d9cec98dc4
Engxli <baaa555@outlook.com>
2024-06-06 17:49:58 +0300

    library setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 function App() {
-  return <div className="bg-red-500">hi</div>;
+  return (
+    <Routes>
+      <Route path="/" Component={Home} />
+      <Route path="/login" Component={Login} />
+    </Routes>
+  );
 }
 
 export default App;
